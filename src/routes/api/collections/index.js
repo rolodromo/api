0524458d1768ca0d51b3a05eeb6c172c88e548f8
guumaster <guuweb@gmail.com>
2018-07-08 11:05:21 +0200

    check owner on collection actions

--- src/routes/api/collections/index.js
+++ src/routes/api/collections/index.js
@@ -9,7 +9,7 @@ const saveItems = require('./saveItems')
 
 const validateToken = require('../../../middlewares/validate_token')
 
-// const checkOwner = require('../../../middlewares/generators/check_owner')
+const checkOwner = require('../../../middlewares/collections/check_owner')
 
 const mandatoryToken = () => validateToken({required: true})
 
@@ -20,11 +20,11 @@ router.get('/', validateToken(), findAll)
 router.post('/', validateToken(), save)
 
 // single collection
-router.post('/:id', validateToken(), save)
 router.get('/:id', validateToken(), findOne)
-router.delete('/:id', mandatoryToken(), remove)
+router.post('/:id', validateToken(), checkOwner, save)
+router.delete('/:id', mandatoryToken(), checkOwner, remove)
 
 // items
-router.post('/:id/items', mandatoryToken(), saveItems)
+router.post('/:id/items', mandatoryToken(), checkOwner, saveItems)
 
 module.exports = router
