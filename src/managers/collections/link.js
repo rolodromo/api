fff95bc8c5b1232b308b6cc1318bcb98839f30d6
guumaster <guuweb@gmail.com>
2018-09-29 10:45:55 +0200

    change collection links

--- src/managers/collections/link.js
+++ src/managers/collections/link.js
@@ -1,7 +1,12 @@
 const config = require('../../../config')
 
+const MAP = {
+  dice: 'dados',
+  sounds: 'sonidos',
+  generators: 'generadores',
+}
 module.exports = (collection = {}) => {
-  collection.link = `/colecciones/${collection.slug}/${collection.id}`
+  collection.link = `/colecciones/${MAP[collection.type]}/${collection.slug}/${collection.id}`
   collection.shortLink = `${config.host}/c/${collection.id}`
   return collection
 }
